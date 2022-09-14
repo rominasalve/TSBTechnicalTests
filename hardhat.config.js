8ef7d35268f47ff7695bf25e2164a8540c712013
romina <rsalveraglio@infuy.com>
2022-09-14 09:39:08 -0300

    saco ropsten vars

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,6 @@ require("@nomicfoundation/hardhat-toolbox");
  * @type import('hardhat/config').HardhatUserConfig
  */
 require("@nomiclabs/hardhat-ethers");
-require("dotenv").config();
-
-const {
-  API_ROPSTEN_URL,
-  PRIVATE_KEY,
-  PRIVATE_KEY_2,
-} = process.env;
 
 module.exports = {
   solidity: {
@@ -23,9 +16,5 @@ module.exports = {
   },
   defaultNetwork: "hardhat", //In order to run tests against it
   networks: {
-     ropsten: {
-      url: API_ROPSTEN_URL,
-      accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY_2}`]
-    },
   }
 };
